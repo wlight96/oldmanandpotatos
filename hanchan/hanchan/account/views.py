b290b2edbd00e883d11dd73d6c5a15dc56bcdd62
wlight96 <wlight96@naver.com>
2021-06-01 01:03:54 +0900

    hanchanmain

---
+++ hanchan/hanchan/account/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['useremail']
+        if User.objects.filter(username = username).exists():
+            return render(request, 'signup.html', {'error' : "이미 존재하는 사용자 입니다." })
+            
+        if password == request.POST['passwordCheck']:
+            user = User.objects.create_user(
+                username, email, password = password
+            )
+            auth.login(request, user)
+            return redirect('/')
+        else :
+            return render(request, 'signup.html',{'error' : "비밀번호가 일치하지 않습니다."})
+    else :
+        return render(request, 'signup.html')
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username = username, password = password)
+        if user is not None :
+            auth.login(request, user)
+            return redirect('/')
+        else :
+            return render(request, 'login.html', {'error': "사용자 이름 혹은 패스워드가 틀렸습니다."})
+    else : 
+        return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
---
+++ hanchan/hanchan/mainpage/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.urls.resolvers import URLPattern
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+]
