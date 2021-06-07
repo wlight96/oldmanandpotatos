b6ff8030ff488440ef8f5a09ca333d78ba152873
wlight96 <wlight96@naver.com>
2021-06-07 10:48:03 +0900

    DB update

--- hanchan/hanchan/account/models.py
+++ hanchan/hanchan/account/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Userpage(models.Model):
-    userid = models.CharField(db_column='userID', primary_key=True, max_length=20)  # Field name made lowercase.
+    userid = models.CharField(primary_key=True, max_length=20)
     useremail = models.CharField(max_length=45)
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=20)
     b_date = models.DateField()
     user_phone = models.IntegerField(blank=True, null=True)
 
     class Meta:
         managed = False
-        db_table = 'userpage'
+        db_table = 'Userpage'
