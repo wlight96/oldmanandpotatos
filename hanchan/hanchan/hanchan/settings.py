205c16068b9bf741726001ef9c1c56e8b39cefb3
jankim7878 <jankim7878@naver.com>
2021-06-07 10:21:02 +0900

    hyeok0607

--- hanchan/hanchan/hanchan/settings.py
+++ hanchan/hanchan/hanchan/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'hanchan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hanchan',
+        'USER': 'root',
+        'PASSWORD': 'kjh7878!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
